dca9751b4d7b486222d3ff2446b4283d47e1f107
Hector Krionas - Lamprou <e.krionas.lamprou@gmail.com>
2022-03-27 21:55:31 +0300

    Started ProductApiController.

---
+++ KappaCreations/Controllers/Api/ProductApiController.cs
@@ -0,0 +1,49 @@
+﻿using KappaCreations.Database;
+using KappaCreations.Models;
+using KappaCreations.Models.Shop.DTOs;
+using KappaCreations.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace KappaCreations.Controllers.Api
+{
+    public class ProductApiController : ApiController
+    {
+        readonly ShopContext _db;
+        readonly Repository<Product> _repo;
+
+        public ProductApiController()
+        {
+            _db = new ShopContext();
+            _repo = new Repository<Product>(_db);
+        }
+        public ProductApiController(ShopContext db)
+        {
+            _db = db;
+            _repo = new Repository<Product>(_db);
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(IEnumerable<ProductDTO>))]
+        public async Task<IHttpActionResult> GetAsync()
+        {
+            var products = await _repo.GetAllAsync();
+            return Ok(products.Select(product => ProductDTO.MapFrom(product)));
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(IEnumerable<ProductDTO>))]
+        public async Task<IHttpActionResult> GetAsync(int id)
+        {
+            var product = await _repo.GetAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(ProductDTO.MapFrom(product));
+        }
+    }
+}
---
+++ KappaCreations/Models/Shop/DTOs/ProductDTO.cs
@@ -0,0 +1,32 @@
+﻿namespace KappaCreations.Models.Shop.DTOs
+{
+    public class ProductDTO : IDataTransferObject<Product>
+    {
+        public int? Id { get; set; }
+        public DesignDTO Design { get; set; }
+        public DesignDTO BackDesign { get; set; }
+        public ProductCategory Category { get; set; }
+        public int DesignerId { get; set; }
+        public int? Upvotes { get; set; }
+        public int? Reports { get; set; }
+
+        public Product Map() => new Product
+        {
+            Design = Design.Map(),
+            BackDesign = BackDesign.Map(),
+            DesignerId = DesignerId,
+            Upvotes = Upvotes ?? 0,
+            Reports = Reports ?? 0,
+        };
+
+        public static ProductDTO MapFrom(Product product) => new ProductDTO
+        {
+            Id = product.Id,
+            Design = DesignDTO.MapFrom(product.Design),
+            BackDesign = DesignDTO.MapFrom(product.BackDesign),
+            DesignerId = product.DesignerId,
+            Upvotes = product.Upvotes,
+            Reports = product.Reports,
+        };
+    }
+}
