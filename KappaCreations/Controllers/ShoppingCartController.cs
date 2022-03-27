0e8870f28240b5ac3e37aeafee35a1876b45d7b7
Panos1996 <panos2171996@gmail.com>
2022-03-27 21:20:07 +0300

    Shopping Cart

---
+++ KappaCreations/Controllers/ShoppingCartController.cs
@@ -0,0 +1,94 @@
+﻿using KappaCreations.Database;
+using KappaCreations.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KappaCreations.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        readonly ShopContext _db;
+        private string strCart = "Cart";
+
+        public ShoppingCartController()
+        {
+            _db = new ShopContext();
+        }
+
+
+        public ShoppingCartController(ShopContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OrderNow(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (Session[strCart] == null)
+            {
+                List<Cart> isCart = new List<Cart>
+                {
+                    new Cart(_db.Products.Find(id),1)
+                };
+
+                Session[strCart] = isCart;
+            }
+            else
+            {
+                List<Cart> isCart = (List<Cart>)Session[strCart];
+                int check = isExistingCheck(id);
+                if (check == -1)
+                {
+                    isCart.Add(new Cart(_db.Products.Find(id), 1));
+                }
+                else
+                {
+                    isCart[check].Quantity++;
+                }
+                Session[strCart] = isCart;
+            }
+
+            return View("~/Views/Home/ShoppingCart.cshtml");
+        }
+
+        private int isExistingCheck(int? id)
+        {
+            List<Cart> isCart = (List<Cart>)Session[strCart];
+            for (int i = 0; i < isCart.Count; i++)
+            {
+                if (isCart[i].Product.Id == id)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            int check = isExistingCheck(id);
+            List<Cart> isCart = (List<Cart>)Session[strCart];
+            isCart.RemoveAt(check);
+            return View("~/Views/Home/ShoppingCart.cshtml");
+        }
+
+    }
+}
---
+++ KappaCreations/Models/Cart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KappaCreations.Models
+{
+    public class Cart
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+        public Cart(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+    }
+}
