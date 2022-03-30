43173d660457f17ad32a4d71c17b11f385ca908c
Christina Adams <chrystal_7_1@hotmail.com>
2022-03-31 00:31:24 +0300

    Gallery controller - gallery links

---
+++ KappaCreations/Controllers/GalleryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KappaCreations.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult GalleryProductDetails()
+        {
+            return View();
+        }
+    }
+}
