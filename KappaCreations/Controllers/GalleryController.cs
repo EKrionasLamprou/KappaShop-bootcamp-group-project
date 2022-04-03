cae0a198db6d62b8149b12fb2857e02023bd9d70
Christina Adams <chrystal_7_1@hotmail.com>
2022-04-03 20:54:45 +0300

    pagination prep

--- KappaCreations/Controllers/GalleryController.cs
+++ KappaCreations/Controllers/GalleryController.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using PagedList.Mvc;
+using PagedList;
 
 namespace KappaCreations.Controllers
 {
@@ -25,9 +27,13 @@ namespace KappaCreations.Controllers
             _repo = new ProductRepository(_db);
         }
         
-        public async Task<ActionResult> Index()
+        public async Task<ActionResult> Index(int? page, int? pSize)
         {
             var products = await _repo.GetAllAsync();
+
+            int pageSize = pSize ?? 4;
+            int pageNumber = page ?? 1;
+            //return View(products.ToPagedList(pageNumber,pageSize));
             return View();
         }
 
