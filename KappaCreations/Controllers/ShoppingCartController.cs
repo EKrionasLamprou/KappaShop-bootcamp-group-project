d87cdb91fa4303acce86ba32c50ce711dc7a6e5b
Panos1996 <panos2171996@gmail.com>
2022-04-07 17:20:38 +0300

    paypal

--- KappaCreations/Controllers/ShoppingCartController.cs
+++ KappaCreations/Controllers/ShoppingCartController.cs
@@ -1,5 +1,6 @@
 ﻿using KappaCreations.Database;
 using KappaCreations.Models;
+using PayPal.Api;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,7 @@ namespace KappaCreations.Controllers
     public class ShoppingCartController : Controller
     {
         readonly ShopContext _db;
-        
+
 
         public ShoppingCartController()
         {
@@ -32,11 +33,11 @@ namespace KappaCreations.Controllers
 
         public ActionResult Buy(int? id)
         {
-            
+
             if (Session["cart"] == null)
             {
                 List<OrderItem> cart = new List<OrderItem>();
-                cart.Add(new OrderItem { Product = _db.Products.ToList().Single(x=> x.Id == id), Quantity = 1 });
+                cart.Add(new OrderItem { Product = _db.Products.ToList().Single(x => x.Id == id), Quantity = 1 });
                 Session["cart"] = cart;
             }
             else
@@ -74,10 +75,139 @@ namespace KappaCreations.Controllers
                 {
                     return i;
                 }
-                    
+
             }
-                
+
             return -1;
+        }
+
+
+
+
+        public ActionResult PaymentWithPaypal(string Cancel = null)
+        {
+            //getting the apiContext
+            APIContext apiContext = PaypalConfiguration.GetAPIContext();
+            try
+            {
+                //A resource representing a Payer that funds a payment Payment Method as paypal
+                //Payer Id will be returned when payment proceeds or click to pay
+                string payerId = Request.Params["PayerID"];
+                if (string.IsNullOrEmpty(payerId))
+                {
+                    //this section will be executed first because PayerID doesn't exist
+                    //it is returned by the create function call of the payment class
+                    // Creating a payment
+                    // baseURL is the url on which paypal sendsback the data.
+                    string baseURI = Request.Url.Scheme + "://" + Request.Url.Authority +
+                    "/ShoppingCart/PaymentWithPayPal?";
+                    //here we are generating guid for storing the paymentID received in session
+                    //which will be used in the payment execution
+                    var guid = Convert.ToString((new Random()).Next(100000));
+                    //CreatePayment function gives us the payment approval url
+                    //on which payer is redirected for paypal account payment
+                    var createdPayment = this.CreatePayment(apiContext, baseURI + "guid=" + guid);
+                    //get links returned from paypal in response to Create function call
+                    var links = createdPayment.links.GetEnumerator();
+                    string paypalRedirectUrl = String.Empty;
+                    while (links.MoveNext())
+                    {
+                        Links link = links.Current;
+                        if (link.rel.ToLower().Trim().Equals("approval_url"))
+                        {
+                            //saving the payapalredirect URL to which user will be redirected for payment
+                            paypalRedirectUrl = link.href;
+                        }
+                    }
+                    // saving the paymentID in the key guid
+                    Session.Add(guid, createdPayment.id);
+                    return Redirect(paypalRedirectUrl);
+                }
+                else
+                {
+                    // This function exectues after receving all parameters for the payment
+                    var guid = Request.Params["guid"];
+                    var executedPayment = ExecutePayment(apiContext, payerId, Session[guid] as string);
+                    //If executed payment failed then we will show payment failure message to user
+                    if (executedPayment.state.ToLower() != "approved")
+                    {
+                        return View("Failure");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return View("Failure");
+            }
+            //on successful payment, show success page to user.
+            return View("~/Views/Home/Index.cshtml");
+        }
+        private PayPal.Api.Payment payment;
+        private Payment ExecutePayment(APIContext apiContext, string payerId, string paymentId)
+        {
+            var paymentExecution = new PaymentExecution() { payer_id = payerId };
+            this.payment = new Payment() { id = paymentId };
+            return this.payment.Execute(apiContext, paymentExecution);
+        }
+        private Payment CreatePayment(APIContext apiContext, string redirectUrl)
+        {
+            //create itemlist and add item objects to it
+            var itemList = new ItemList() { items = new List<Item>() };
+
+            List<OrderItem> listCarts = (List<OrderItem>)Session["cart"];
+
+            foreach (var cart in listCarts)
+            {
+                itemList.items.Add(new Item()
+                {
+                    name = cart.Product.Category.Title,
+                    currency = "EUR",
+                    price = cart.Product.Category.Price.ToString(),
+                    quantity = cart.Quantity.ToString(),
+                    sku = "sku"
+                });
+            }
+            //Adding Item Details like name, currency, price etc
+
+            var payer = new Payer() { payment_method = "paypal" };
+            // Configure Redirect Urls here with RedirectUrls object
+            var redirUrls = new RedirectUrls()
+            {
+                cancel_url = redirectUrl + "&Cancel=true",
+                return_url = redirectUrl
+            };
+            // Adding Tax, shipping and Subtotal details
+            var details = new Details()
+            {
+                tax = "1",
+                shipping = "2",
+                subtotal = listCarts.Sum(x => x.Quantity * x.Product.Category.Price).ToString()
+            };
+            //Final amount with details
+            var amount = new Amount()
+            {
+                currency = "EUR",
+                total = (Convert.ToDouble(details.tax) + Convert.ToDouble(details.shipping) + Convert.ToDouble(details.subtotal)).ToString(), // Total must be equal to sum of tax, shipping and subtotal.
+                details = details
+            };
+            var transactionList = new List<Transaction>();
+            // Adding description about the transaction
+            transactionList.Add(new Transaction()
+            {
+                description = "Transaction description",
+                invoice_number = Convert.ToString((new Random()).Next(100000)), //Generate an Invoice No
+                amount = amount,
+                item_list = itemList
+            });
+            this.payment = new Payment()
+            {
+                intent = "sale",
+                payer = payer,
+                transactions = transactionList,
+                redirect_urls = redirUrls
+            };
+            // Create a payment using a APIContext
+            return this.payment.Create(apiContext);
         }
 
 
