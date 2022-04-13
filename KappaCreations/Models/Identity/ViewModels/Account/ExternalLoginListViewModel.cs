d39b68263fb5defaca4d28535f18e85a593db802
Christina Adams <chrystal_7_1@hotmail.com>
2022-04-14 00:38:13 +0300

    so that we can log in and have a username

--- KappaCreations/Models/Identity/ViewModels/Account/ExternalLoginListViewModel.cs
+++ KappaCreations/Models/Identity/ViewModels/Account/ExternalLoginListViewModel.cs
@@ -5,6 +5,10 @@ namespace KappaCreations.Models
     public class ExternalLoginConfirmationViewModel
     {
         [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
         [Display(Name = "Email")]
         public string Email { get; set; }
     }
