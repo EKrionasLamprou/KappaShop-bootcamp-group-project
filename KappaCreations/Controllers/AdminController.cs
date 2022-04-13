cab6c00240c0180031ae9c274088986c57fe73ae
Christos Karamolegkos <c.karamolegkos@gmail.com>
2022-04-13 20:27:57 +0300

    admin

--- KappaCreations/Controllers/AdminController.cs
+++ KappaCreations/Controllers/AdminController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace KappaCreations.Controllers
 {
-    [Authorize]
+    [Authorize(Roles ="Admin")]
     public class AdminController : Controller
     {
         // GET: Admin
