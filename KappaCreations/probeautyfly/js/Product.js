eed13b2633e1b93b2567eaa3d85080b73ac95cc2
AnnaKitou <null_number_13@hotmail.com>
2022-04-18 22:05:55 +0300

    gallery fixes

--- KappaCreations/probeautyfly/js/Product.js
+++ KappaCreations/probeautyfly/js/Product.js
@@ -7,6 +7,7 @@
             "Content-Type": "application/json",
         },
         success: function (response) {
+            console.log(response)
             const canvas = (this.__canvas = new fabric.Canvas("canvas"));
             fillCanvas(canvas, response);
         },
@@ -48,11 +49,11 @@ function mapTextToCanvas(t) {
             type: "text",
             originX: "left",
             originY: "top",
-            left: t.posX/3.5,
-            top: t.posY /3.5,
+            left: (t.posX / 3.5) + 50,
+            top: t.posY / 3.5,
             width: t.sizeWidth / 3.5,
             height: t.sizeHeight / 3.5,
-            fill: "#"+t.colourHex,
+            fill: "#" + t.colourHex,
             stroke: null,
             strokeWidth: 1,
             strokeDashArray: null,
@@ -85,39 +86,39 @@ function mapTextToCanvas(t) {
     }
 }
 
-    function mapImageToCanvas(image) {
-        return {
-            data: {
-                type: "image",
-                originX: "left",
-                originY: "top",
-                left: image.posX / 3.5,
-                top: image.posY / 3.5,
-                width: image.sizeWidth / 3.5,
-                height: image.sizeHeight / 3.5,
-                fill: "rgb(0,0,0)",
-                stroke: null,
-                strokeWidth: 1,
-                strokeDashArray: null,
-                strokeLineCap: "butt",
-                strokeLineJoin: "miter",
-                strokeMiterLimit: 10,
-                scaleX: 1,
-                scaleY: 1,
-                angle: 0,
-                flipX: false,
-                flipY: false,
-                opacity: image.colourAlpha,
-                shadow: null,
-                visible: true,
-                clipTo: null,
-                backgroundColor: "",
-                src: image.url,
-                filters: [{ type: "Tint", color: image.colourHex, opacity: 0.5 }],
-                crossOrigin: ""
-            },
-            zIndex: image.zIndex,
-        }
+function mapImageToCanvas(image) {
+    return {
+        data: {
+            type: "image",
+            originX: "left",
+            originY: "top",
+            left: (image.posX / 3.5) + 50,
+            top: image.posY / 3.5,
+            width: image.sizeWidth / 3.5,
+            height: image.sizeHeight / 3.5,
+            fill: "rgb(0,0,0)",
+            stroke: null,
+            strokeWidth: 1,
+            strokeDashArray: null,
+            strokeLineCap: "butt",
+            strokeLineJoin: "miter",
+            strokeMiterLimit: 10,
+            scaleX: 1,
+            scaleY: 1,
+            angle: 0,
+            flipX: false,
+            flipY: false,
+            opacity: image.colourAlpha,
+            shadow: null,
+            visible: true,
+            clipTo: null,
+            backgroundColor: "",
+            src: image.url,
+            filters: [{ type: "Tint", color: image.colourHex, opacity: 0.5 }],
+            crossOrigin: ""
+        },
+        zIndex: image.zIndex,
     }
+}
 
 document.addEventListener("DOMContentLoaded", () => { setApiCall() });
